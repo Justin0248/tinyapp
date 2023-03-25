4d4904b8dcd0c3eb24c3894afb8dd02b20f4b9fd
Yonin408 <justin12689@gmail.com>
2023-03-24 19:32:43 -0700

    bug fixes: fixed bug where entering a new url would do nothing

--- express_server.js
+++ express_server.js
@@ -7,14 +7,13 @@ app.set('view engine', 'ejs');
 app.use(cookieParser());
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "b2xVn2": "http://www.lighthouselabs.ca"
+  
 };
-console.log(urlDatabase)
+console.log(urlDatabase);
 const users = {
-
 };
-console.log(users)
 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
 
 
@@ -29,10 +28,63 @@ return output;
 app.use(express.urlencoded({ extended: true }));
 
 
+
+
+
+//generates a random short url to associate 
+//with the created url and adds it to home page
+app.post("/urls/new", (req, res) => {
+  const longURL = req.body.longURL;
+  if (longURL) {
+    const id = generateRandomString();
+    urlDatabase[id] = longURL; 
+    res.redirect(`/urls`);
+  }
+});
+
+
+//lets user edit a long url without changing the short url
+app.post("/urls/:id/edit", (req, res) => {
+  const id = req.params.id;
+  const eLongURL = req.body.eLongURL;
+  if (eLongURL){
+  urlDatabase[id] = eLongURL;
+  }
+  res.redirect(`/urls/${id}`);
+});
+
+
+//lets user sign in/up using username
+app.post("/urls", (req, res) => {
+ let username = req.body["username"]
+ if (username) {
+  res.cookie('username', username);
+  res.redirect("/urls");
+ }
+});
+
+
+//lets user logout
+app.post("/urls/:username", (req, res) => {
+let username = req.cookies["username"];
+res.clearCookie('username', username);
+res.redirect("/urls");
+});
+
+
+//lets user delete a url from the home page
+app.post("/urls/:id/delete", (req, res) => {
+  const id = req.params.id;
+delete urlDatabase[id];
+res.redirect("/urls");
+});
+
+
 //shows username in create new url header
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"]
+    username: req.cookies["username"],
+    urls: urlDatabase
   }
   res.render("urls_new", templateVars);
 });
@@ -43,7 +95,7 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = { 
     id: req.params.id, 
     longURL: urlDatabase[req.params.id],
-    username: req.cookies["username"] 
+    username: req.cookies["username"]
   };
   res.render("urls_show", templateVars);
 });
@@ -52,8 +104,8 @@ app.get("/urls/:id", (req, res) => {
 //shows username in url homepage
 app.get("/urls", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
-    urls: urlDatabase
+    urls: urlDatabase,
+    username: req.cookies["username"]
   };
   res.render("urls_index", templateVars);
 });
@@ -75,65 +127,6 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
-
-//page to let user register using email and password
-app.post("/registration", (req, res) => {
-const email = req.body.email;
-const password = req.body.password;
-const userID = generateRandomString()
-users[userID] = {
-  id: userID,
-email: email,
-password: password
-};
-res.cookie("userID", users)
-res.redirect("/urls");
-});
-
-
-//lets user sign in/up using username
-app.post("/urls", (req, res) => {
- let username = req.body["username"]
-  res.cookie('username', username);
-  res.redirect("/urls");
-});
-
-
-//lets user logout
-app.post("/urls/:username", (req, res) => {
-let username = req.cookies["username"];
-res.clearCookie('username', username);
-res.redirect("/urls");
-});
-
-
-//generates a random short url to associate 
-//with the created url and adds it to home page
-app.post("/urls/new", (req, res) => {
-  const longURL = req.body.longURL;
-  const shortURL = generateRandomString();
-  urlDatabase[shortURL] = longURL; 
-  res.redirect(`/urls/${shortURL}`);
-});
-
-
-//lets user edit a long url without changing the short url
-app.post("/urls/:id/edit", (req, res) => {
-  const id = req.params.id;
-  const longURL = req.body.longURL;
-  if (longURL){
-  urlDatabase[id] = longURL;
-  }
-  res.redirect(`/urls/${id}`);
-});
-
-
-//lets user delete a url from the home page
-app.post("/urls/:id/delete", (req, res) => {
-  const id = req.params.id;
-delete urlDatabase[id];
-res.redirect("/urls");
-});
 
 
 
