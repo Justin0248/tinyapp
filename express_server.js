24400876b3c91878b91c48f8f19f1678892da683
Yonin408 <justin12689@gmail.com>
2023-03-26 01:40:40 -0700

    cleaned up code and edited comments

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const cookieSession = require('cookie-session')
-const {getUserByEmail} = require('./helper')
+const cookieSession = require('cookie-session');
+const {getUserByEmail} = require('./helper');
 const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(cookieSession({
   name: 'session',
   keys: ['secret', 'adsfgiuhawef'],
-}))
+}));
 
 const urlDatabase = {
 
@@ -38,32 +38,28 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-//login page
+//login function
 app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const hashedPassword = req.session.hashedPassword
-  const uid = getUserByEmail(email, users)
-if (uid && bcrypt.compareSync(password, hashedPassword)) {
-  for (const keys in users) {
-    if (users[keys].email === email && users[keys].password === password) {
-      const id = users[keys].id;
-      console.log(users);
-      req.session.email = email
-      // res.cookie('email', email);
-      req.session.hashedPassword = hashedPassword
-      // res.cookie('hashedPassword', hashedPassword);
-      req.session.id = id
-      // res.cookie('id', id);
-      res.redirect("/urls");
+  const hashedPassword = req.session.hashedPassword;
+  const uid = getUserByEmail(email, users);
+  if (uid && bcrypt.compareSync(password, hashedPassword)) {
+    for (const keys in users) {
+      if (users[keys].email === email && users[keys].password === password) {
+        const id = users[keys].id;
+        req.session.email = email;
+        req.session.hashedPassword = hashedPassword;
+        req.session.id = id;
+        res.redirect("/urls");
+      }
     }
   }
-}
   res.status(403).send('Error, wrong information provided');
 });
   
   
-//registration page
+//registration function
 app.post("/registration", (req, res) => {
   let id = generateRandomString();
   const email = req.body.email;
@@ -78,17 +74,15 @@ app.post("/registration", (req, res) => {
   if (!email || !password) {
     res.status(400).send('Error, insufficient information provided');
   } else if (email && password) {
-    const hashedPassword = bcrypt.hashSync(password, 10)
+    const hashedPassword = bcrypt.hashSync(password, 10);
     users[id] = {
       id,
       email: email,
       password: password
     };
-    req.session.email = email
-    // res.cookie('email', email);
-    req.session.hashedPassword = hashedPassword
-    // res.cookie('hashedPassword', hashedPassword);
-    req.session.id = id
+    req.session.email = email;
+    req.session.hashedPassword = hashedPassword;
+    req.session.id = id;
     // res.cookie('id', id);
     res.redirect("/urls");
   }
@@ -100,14 +94,13 @@ app.post("/registration", (req, res) => {
 //with the created url and adds it to home page
 app.post("/urls/new", (req, res) => {
   const longURL = req.body.longURL;
-  const ids = req.session.id
+  const ids = req.session.id;
   if (longURL) {
     const id = generateRandomString();
     urlDatabase[id] = {
       longURL: longURL,
       userID: ids,
     };
-    console.log(urlDatabase);
     res.redirect(`/urls`);
   }
 });
@@ -130,23 +123,27 @@ app.post("/urls", (req, res) => {
   let password = req.session.password;
   if (email) {
     req.session.email = undefined;
-    // res.clearCookie('email', email);
     req.session.password = undefined;
-    // res.clearCookie('password', password);
     res.redirect("/login");
   }
 });
 
 
-//lets user delete a url from the home page
+//lets user delete a url from the home page, only if they created the url
 app.post("/urls/:id/delete", (req, res) => {
+  const uid = req.session.id;
+  for (key in urls) {
+    if (uid !== urls[key].userID) {
+      res.redirect('/urls');
+    }
+  }
   const id = req.params.id;
   delete urlDatabase[id];
   res.redirect("/urls");
 });
 
 
-//shows username in create new url header
+//gives user the page to create a new url
 app.get("/urls/new", (req, res) => {
   const email = req.session.email;
   if (!email) {
@@ -161,20 +158,20 @@ app.get("/urls/new", (req, res) => {
 });
 
 
-//shows username in header while in edit url
+//gives user edit page for selected url, only if they made the url
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   if (!urlDatabase[id]) {
     res.status(400).send('url does not exist');
   }
-const urls = urlDatabase;
-const uid = req.session.id
+  const urls = urlDatabase;
+  const uid = req.session.id;
 
-for (key in urls) {
-  if (uid !== urls[key].userID) {
-    res.redirect('/urls')
+  for (key in urls) {
+    if (uid !== urls[key].userID) {
+      res.redirect('/urls');
+    }
   }
-}
   const templateVars = {
     id,
     urls,
@@ -186,11 +183,11 @@ for (key in urls) {
 });
 
 
-//shows username in url homepage
+//gives user homepage if they are signed in
 app.get("/urls", (req, res) => {
   email = req.session.email;
   if (!email) {
-    res.status(400).send('please login first')
+    res.status(400).send('please login first');
   }
   const templateVars = {
     urls: urlDatabase,
@@ -202,6 +199,7 @@ app.get("/urls", (req, res) => {
 });
 
 
+//gives user the login page
 app.get("/login", (req, res) => {
   const templateVars = {
     user: users,
@@ -233,6 +231,12 @@ app.get("/u/:id", (req, res) => {
 });
 
 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+
+
 
 
 //test function
@@ -246,13 +250,8 @@ app.get("/urls.json", (req, res) => {
 });
 
 
-
-
-  
+//test function
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
